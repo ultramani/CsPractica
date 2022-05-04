2d7a394a99c70000d0716d747e837fc730e2d1bb
adroius <74370106+adroius@users.noreply.github.com>
2022-05-04 09:22:25 +0200

    Update Singup.java

--- App/app/src/main/java/com/example/myfirstjob/data/Singup.java
+++ App/app/src/main/java/com/example/myfirstjob/data/Singup.java
@@ -38,5 +38,13 @@ public class Singup {
         }
         return false;
     }
+    
+    public boolean comprobationPattern (String email) {
+    Pattern pattern = Pattern.compile("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
+    Matcher mat = pattern.matcher(email);
+    boolean match= mat.matches();
+    return match;
+    }
 
 }
