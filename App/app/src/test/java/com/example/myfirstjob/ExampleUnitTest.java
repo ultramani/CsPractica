c1fed4a1335c2ccbec071ad2cf94e351de47dda1
adroius <74370106+adroius@users.noreply.github.com>
2022-05-04 09:24:51 +0200

    Update ExampleUnitTest.java

--- App/app/src/test/java/com/example/myfirstjob/ExampleUnitTest.java
+++ App/app/src/test/java/com/example/myfirstjob/ExampleUnitTest.java
@@ -10,8 +10,32 @@ import org.junit.Test;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void comprobationPattern_isCorrect() {
+        Singup singup = new Singup();
+        String emailGood = "paco@gmail.com";
+        String emailBad = "pacoPata7i62equdq2";
+        boolean condGood = singup.comprobationPattern(emailGood);
+        boolean condBad = singup.comprobationPattern(emailBad);
+        assertTrue(condGood && !condBad);
+    }
+    
+    @Test
+    public void encryptPassword_isCorrect() {
+        String passwordString = "1234A";
+        String expectedPass = "N49rLzJed91dElP4DH9GgQ==";
+        String pass = TypePassword.encryptPassword(passwordString);
+
+        assertEquals(expectedPass, pass);
+    }
+
+    @Test
+    public void changePassword_isCorrect() {
+        String oldPassword = "1234A";
+        User user = new User("Paco", "paco@gmail.com", oldPassword, null, "51649875B", "GIS", "URJC");
+        String newPassword = "CalidadSoftware";
+
+        assertTrue(user.changePassword(TypePassword.encryptPassword(oldPassword), newPassword, newPassword));
     }
 }
