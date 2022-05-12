86a2e61b9bdbf43189d39f263d35466dbfc5571d
juanl <juanluis123.rico@gmail.com>
2022-05-12 14:44:08 +0200

    Modo nocturno

--- App/app/src/main/java/com/example/myfirstjob/ui/WelcomeScreen.java
+++ App/app/src/main/java/com/example/myfirstjob/ui/WelcomeScreen.java
@@ -3,8 +3,10 @@ package com.example.myfirstjob.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.myfirstjob.R;
 
@@ -14,6 +16,24 @@ public class WelcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        final Switch swi = findViewById(R.id.switchNocturno);
+
+        swi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(swi.isChecked()){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    getDelegate().applyDayNight();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    getDelegate().applyDayNight();
+                }
+            }
+        });
+
+
+
     }
 
     /**
